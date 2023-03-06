3f9fac79d28939181e3ad2a79f7e4e4a98d7097b
Alexander Nole <cnole_azabache@outlook.com>
2023-03-06 10:30:00 -0500

    first commit

---
+++ src/main/java/com/develtrex/ClientController.java
@@ -0,0 +1,30 @@
+package com.develtrex;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ClientController {
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${message.welcome}")
+    private String message;
+
+    @GetMapping("config-client-dev")
+    public Response getConfigClient(){
+        var response = new Response();
+        response.setMessage(message);
+        response.setUsername(userName);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientController.class,args);
+    }
+}
---
+++ src/main/java/com/develtrex/Response.java
@@ -0,0 +1,22 @@
+package com.develtrex;
+
+public class Response {
+    private String message;
+    private String username;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
