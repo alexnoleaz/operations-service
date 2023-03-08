9ac10abe95ccee5518e130969a123622ceaa053f
Alexander Nole <cnole_azabache@outlook.com>
2023-03-08 11:02:17 -0500

    Implement feign client

--- src/main/java/com/develtrex/OperationsController.java
+++ src/main/java/com/develtrex/OperationsController.java
@@ -1,32 +1,35 @@
 package com.develtrex;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ClientController {
+@EnableFeignClients
+public class OperationsController {
     @Value("${user.name}")
     private String userName;
 
     @Value("${message.welcome}")
     private String message;
 
-    @GetMapping("config-client-dev")
-    public Response getConfigClient() {
-        var response = new Response();
-        response.setMessage(message);
-        response.setUsername(userName);
+    @Autowired
+    private ResultsClientRest resultsClientRest;
 
-        return response;
+    @GetMapping("operations/{a}/{b}")
+    public String getOperations(@PathVariable float a, @PathVariable float b){
+        return resultsClientRest.showResult(a-b);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientController.class, args);
+        SpringApplication.run(OperationsController.class, args);
     }
 }
