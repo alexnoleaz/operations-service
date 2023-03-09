73f00fcf10253015564f091b4c3f9b5b864a2e71
Alexander Nole <cnole_azabache@outlook.com>
2023-03-09 09:25:04 -0500

    Circuit Breaker with Hystrix
    Dependencies and versions:
    
    - spring-boot-starter -> 2.3.13.RELEASE
    - spring-cloud-dependencies -> Hoxton.SR12
    - spring-cloud-starter-netflix-hystrix

--- src/main/java/com/develtrex/OperationsController.java
+++ src/main/java/com/develtrex/OperationsController.java
@@ -1,9 +1,11 @@
 package com.develtrex;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OperationsController {
     @Value("${user.name}")
     private String userName;
@@ -24,6 +27,7 @@ public class OperationsController {
     @Autowired
     private ResultsClientRest resultsClientRest;
 
+    @HystrixCommand(fallbackMethod = "fallbackError")
     @GetMapping("operations/{a}/{b}")
     public String getOperations(@PathVariable float a, @PathVariable float b){
         return resultsClientRest.showResult(a-b);
@@ -31,5 +35,9 @@ public class OperationsController {
 
     public static void main(String[] args) {
         SpringApplication.run(OperationsController.class, args);
+    }
+
+    private String fallbackError(float a, float b){
+        return "We are having server problems, please try again at a later time.";
     }
 }
--- src/main/java/com/develtrex/ResultsClientRest.java
+++ src/main/java/com/develtrex/ResultsClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "config-client-two", url = "http://localhost:8080")
+@FeignClient(name = "results-service", url = "http://localhost:8080")
 public interface ResultsClientRest {
     @GetMapping("results/{number}")
     String showResult(@PathVariable float number);
